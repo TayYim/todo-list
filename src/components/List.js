2ae713acdba5305d5ae0933b51a5a74a40d40a04
Jocularity_Senescence <810906903@qq.com>
2022-04-27 17:21:56 +0800

    add onClick event handler for delete button

--- src/components/List.js
+++ src/components/List.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
-export const List = ({ todoItems }) => {
-	const list = todoItems.map((element, index) => <li key={index}>{element}</li>);
+export const List = ({ todoItems, setTodoItems }) => {
+	const handleDeleteItem = (event) => {
+		event.preventDefault();
+		const orderOfItemToBeDeleted = parseInt(event.target.getAttribute("order"));
+		console.log(`The order of item to be delted in state is: ${orderOfItemToBeDeleted}`);
+		console.log("please delete me!");
+	};
+
+	const list = todoItems.map((element, index) => 
+		<li key={index}>
+			{element}
+			<button order={index} onClick={handleDeleteItem}>x</button>
+		</li>);
 
 	return <ul>{list}</ul>;
 };
